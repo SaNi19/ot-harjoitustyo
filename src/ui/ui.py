63a468374137ec7f125dea01f078c80a30a0c6fd
Sari <51118190+SaNi19@users.noreply.github.com>
2022-12-05 18:26:41 +0200

    ui korjattu

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,86 +1,43 @@
-from tkinter import Tk, constants, ttk
-
+from tkinter import Tk
+from ui.login_view import LoginView
+from ui.storage_view import StorageView
 
 class UI:
     def __init__(self, root):
         self._root = root
-        self._entry = None
+        self._current_view = None
 
     def start(self):
+        self._show_hello_view()
 
-        login_heading_label = ttk.Label(master=self._root, text="Login")
+    def _hide_current_view(self):
+        if self._current_view:
+            self._current_view.destroy()
 
-        login_username_label = ttk.Label(master=self._root, text="Username")
-        login_username_entry = ttk.Entry(master=self._root)
+        self._current_view = None
 
-        login_password_label = ttk.Label(master=self._root, text="Password")
-        login_password_entry = ttk.Entry(master=self._root)
+    def _handle_good_bye(self):
+        self._show_good_bye_view()
 
-        login_button = ttk.Button(
-            master=self._root, text="Login", command=self.__handle_button_click)
+    def _handle_hello(self):
+        self._show_hello_view()
 
-        create_heading_label = ttk.Label(
-            master=self._root, text="Create a new account")
+    def _show_hello_view(self):
+        self._hide_current_view()
 
-        create_username_label = ttk.Label(master=self._root, text="Username")
-        create_username_entry = ttk.Entry(master=self._root)
+        self._current_view = LoginView(
+            self._root,
+            self._handle_good_bye
+        )
 
-        create_password1_label = ttk.Label(master=self._root, text="Password")
-        create_password1_entry = ttk.Entry(master=self._root)
+        self._current_view.pack()
 
-        create_password2_label = ttk.Label(
-            master=self._root, text="Confirm password")
-        create_password2_entry = ttk.Entry(master=self._root)
+    def _show_good_bye_view(self):
+        self._hide_current_view()
 
-        create_button = ttk.Button(master=self._root, text="Registration")
+        self._current_view = StorageView(
+            self._root,
+            self._handle_hello
+        )
 
-        login_heading_label.grid(row=0, column=1, columnspan=2, sticky=(
-            constants.E, constants.W), padx=7, pady=7)
-
-        login_username_label.grid(
-            row=1, column=0, sticky=constants.W, padx=5, pady=5)
-        login_username_entry.grid(row=1, column=1, sticky=(
-            constants.E, constants.W), padx=5, pady=5)
-
-        login_password_label.grid(
-            row=2, column=0, sticky=constants.W, padx=5, pady=5)
-        login_password_entry.grid(row=2, column=1, sticky=(
-            constants.E, constants.W), padx=5, pady=5)
-
-        login_button.grid(row=3, column=1, columnspan=2,
-                          sticky=constants.W, padx=5, pady=5)
-
-        create_heading_label.grid(row=4, column=1, columnspan=2, sticky=(
-            constants.E, constants.W), padx=7, pady=7)
-
-        create_username_label.grid(
-            row=5, column=0, sticky=constants.W, padx=5, pady=5)
-        create_username_entry.grid(row=5, column=1, sticky=(
-            constants.E, constants.W), padx=5, pady=5)
-
-        create_password1_label.grid(
-            row=6, column=0, sticky=constants.W, padx=5, pady=5)
-        create_password1_entry.grid(row=6, column=1, sticky=(
-            constants.E, constants.W), padx=5, pady=5)
-
-        create_password2_label.grid(
-            row=7, column=0, sticky=constants.W, padx=5, pady=5)
-        create_password2_entry.grid(row=7, column=1, sticky=(
-            constants.E, constants.W), padx=5, pady=5)
-
-        create_button.grid(row=8, column=1, columnspan=2,
-                           sticky=constants.W, padx=5, pady=5)
-        self._root.grid_columnconfigure(1, weight=1)
-
-    def __handle_button_click(self):
-        entry_value = self._entry.get()
-        print(f"Value of entry is: {entry_value}")
-
-
-window = Tk()
-window.title("Wood storage")
-
-ui = UI(window)
-ui.start()
-
-window.mainloop()
+        self._current_view.pack()
