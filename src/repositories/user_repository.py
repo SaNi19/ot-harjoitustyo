bd4f888584669959965f84148f96ca2965ce1702
Sari <51118190+SaNi19@users.noreply.github.com>
2022-12-06 13:44:03 +0200

    repository lisätty

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -1,1 +1,17 @@
+import os
+
+class UserRepository:
+    def __init__(self, data):
+        self.__data = data
+
+    def load(self):
+        usernames = {}
+        with open(self.__data) as f:
+            for row in f:
+                bit = row.strip().split(':')
+                username, * name = bit
+                usernames[username] = name
+
+
+            return usernames
 
