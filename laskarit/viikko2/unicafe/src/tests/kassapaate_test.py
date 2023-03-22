1b81520d08265b4c292c7221837b4bad0f34fe96
Sari <sarimarjaana@gmail.com>
2023-03-22 21:53:18 +0200

    Viikko2 maksukorttitestit

---
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -0,0 +1,7 @@
+import unittest
+from kassapaate import Kassapaate
+
+
+class TestKassapaate(unittest.TestCase):
+    def setUp(self):
+        self.kassapaate = Kassapaate(100000)
--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,30 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikei(self):
+        self.maksukortti.lataa_rahaa(2500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 35.00 euroa")
+
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+        self.maksukortti.ota_rahaa(250)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 7.50 euroa",)
+
+    def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
+        self.maksukortti.ota_rahaa(400)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 6.00 euroa")
+
+    def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(200)
+        kortti.ota_rahaa(300)
+
+        self.assertEqual(str(kortti), "Kortilla on rahaa 2.00 euroa")
+
+    
+   
