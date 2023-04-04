86250317aaca4738c75a73d484419f70808ea560
Sari <sarimarjaana@gmail.com>
2023-04-04 21:26:15 +0300

    game-app päivitetty

--- game-app/src/index.py
+++ game-app/src/index.py
@@ -1,24 +1,23 @@
 import pygame
-class Sokoban:
+class MySokoban:
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption("Sokoban")
+        pygame.display.set_caption("My Sokoban")
 
-        self.load_images()
+        self.images()
         self.game()
         self.height = len(self.map)
         self.width = len(self.map[0])
         self.part = self.images[0].get_width()
-
         gamemap_height = self.part * self.height
         gamemap_width = self.part * self.width
         self.sreen = pygame.display.set_mode((gamemap_width, gamemap_height))
 
         self.loop()
 
-    def load_images(self):
+    def images(self):
         self.images = []
-        for name in ["floor", "wall", "pallo", "maali", "otus"]:
+        for name in ["floor", "wall", "ball", "place", "player"]:
             self.images.append(pygame.image.load(name + ".png"))
 
     def game(self):
@@ -27,29 +26,30 @@ class Sokoban:
                     [1, 0, 0, 2, 2, 1, 0, 0, 0, 0, 2, 0, 1, 1, 0, 1, 1],
                     [1, 0, 0, 0, 0, 1, 0, 1, 1, 2, 2, 0, 1, 3, 3, 3, 1],
                     [1, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 1],
-		    [1, 0, 0, 0, 2, 2, 1, 2, 0, 1, 1, 1, 1, 0, 0, 3, 1],
+		            [1, 0, 0, 0, 2, 2, 1, 2, 0, 1, 1, 1, 1, 0, 0, 3, 1],
                     [1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 3, 3, 3, 1],
                     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]] 
         
-    def loop(self):
-        while True:
-            self.tutki_tapahtumat()
-            self.piirra_naytto()
+    
 
-    def tutki_tapahtumat(self):
-        for tapahtuma in pygame.event.get():
-            if tapahtuma.type == pygame.QUIT:
+    def event(self):
+        for command in pygame.event.get():
+            if command.type == pygame.QUIT:
                 exit()
 
-    def piirra_naytto(self):
+    def display_game(self):
         self.sreen.fill((0, 0, 0))
-
-        for y in range(self.height):
-            for x in range(self.width):
-                bit = self.map[y][x]
-                self.sreen.blit(self.images[bit], (x * self.part, y * self.part))
+        for y in range(self.width):
+            for x in range(self.height):
+                bit = self.map[x][y]
+                self.sreen.blit(self.images[bit], (y * self.part, x * self.part))
 
         pygame.display.flip()
+    
+    def loop(self):
+        while True:
+            self.event()
+            self.display_game()
 
 if __name__ == "__main__":
-    Sokoban()
+    MySokoban()
