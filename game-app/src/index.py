64c6e08853305e6c7a3775fd07a7dba1979e4acb
Sari <sarimarjaana@gmail.com>
2023-04-11 21:19:02 +0300

    game-app päivitetty

--- game-app/src/index.py
+++ game-app/src/index.py
@@ -1,54 +1,110 @@
 import pygame
+
 class MySokoban:
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption("My Sokoban")
+        pygame.display.set_caption("MySokoban")
 
         self.images()
         self.game()
+
         self.height = len(self.map)
-        self.width = len(self.map[0])
-        self.part = self.images[0].get_width()
+        self.width = len(self.map[1])
+        self.part = self.images[1].get_width()
+
         gamemap_height = self.part * self.height
         gamemap_width = self.part * self.width
-        self.sreen = pygame.display.set_mode((gamemap_width, gamemap_height))
+        self.sreen = pygame.display.set_mode((gamemap_width, gamemap_height + self.part))
 
         self.loop()
 
     def images(self):
         self.images = []
-        for name in ["floor", "wall", "ball", "place", "player"]:
+        for name in ["floor", "wall", "place", "ball", "player", "valmis", "kohderobo"]:
             self.images.append(pygame.image.load(name + ".png"))
 
-    def game(self):
-        self.map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 4, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1],
-                    [1, 0, 0, 2, 2, 1, 0, 0, 0, 0, 2, 0, 1, 1, 0, 1, 1],
-                    [1, 0, 0, 0, 0, 1, 0, 1, 1, 2, 2, 0, 1, 3, 3, 3, 1],
-                    [1, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 1],
-		            [1, 0, 0, 0, 2, 2, 1, 2, 0, 1, 1, 1, 1, 0, 0, 3, 1],
-                    [1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 3, 3, 3, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]] 
-        
-    
 
-    def event(self):
+    def game(self):
+        self.map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                       [1, 4, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1],
+                       [1, 0, 0, 3, 1, 0, 3, 0, 0, 3, 0, 1, 1, 0, 1, 1],
+                       [1, 0, 0, 0, 1, 3, 1, 1, 3, 3, 0, 1, 2, 2, 2, 1],
+                       [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 1],
+		                [1, 0, 3, 0, 3, 1, 3, 0, 1, 1, 1, 1, 0, 0, 2, 1],
+                       [1, 0, 1, 1, 0, 0, 0, 0, 0, 3, 0, 0, 2, 2, 2, 1],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+        self.siirrot = 0    
+
+
+    def events(self):
         for command in pygame.event.get():
+            if command.type == pygame.KEYDOWN:
+                if command.key == pygame.K_LEFT:
+                    self.move(0, -1)
+                if command.key == pygame.K_RIGHT:
+                    self.move(0, 1)
+                if command.key == pygame.K_UP:
+                    self.move(-1, 0)
+                if command.key == pygame.K_DOWN:
+                    self.move(1, 0)
+                if command.key == pygame.K_ESCAPE:
+                    exit()
             if command.type == pygame.QUIT:
                 exit()
 
+
+    def move(self, y, x):
+        if self.peli_lapi():
+            return
+
+        old_player_y, old_player_x = self.find()
+        new_player_y = old_player_y + y
+        new_player_x = old_player_x + x
+
+        if self.map[new_player_y][new_player_x] == 1:
+            return
+
+        if self.map[new_player_y][new_player_x] in [3, 5]:
+            new_ball_y = new_player_y + y
+            new_ball_x = new_player_x + x
+
+            if self.map[new_ball_y][new_ball_x] in [1, 3, 5]:
+                return
+
+            self.map[new_player_y][new_player_x] -= 3
+            self.map[new_ball_y][new_ball_x] += 3
+
+        self.map[old_player_y][old_player_x] -= 4
+        self.map[new_player_y][new_player_x] += 4
+
+        self.siirrot += 1
+
+    def find(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.map[y][x] in [4, 6]:
+                    return (y, x)
+
     def display_game(self):
-        self.sreen.fill((0, 0, 0))
-        for y in range(self.width):
-            for x in range(self.height):
-                bit = self.map[x][y]
-                self.sreen.blit(self.images[bit], (y * self.part, x * self.part))
+        self.sreen.fill((100, 0, 255))
+
+        for y in range(self.height):
+            for x in range(self.width):
+                ruutu = self.map[y][x]
+                self.sreen.blit(self.images[ruutu], (x * self.part, y * self.part))
 
         pygame.display.flip()
+
+    def peli_lapi(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.map[y][x] in [2, 6]:
+                    return False
+        return True
     
     def loop(self):
         while True:
-            self.event()
+            self.events()
             self.display_game()
 
 if __name__ == "__main__":
