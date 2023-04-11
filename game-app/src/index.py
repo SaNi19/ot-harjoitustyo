e8fb67ccd374d721a0e2021e4e3a1262a5a6f4f3
Sari <sarimarjaana@gmail.com>
2023-04-11 22:41:43 +0300

    game-app päivitetty

--- game-app/src/index.py
+++ game-app/src/index.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class MySokoban:
     def __init__(self):
         pygame.init()
@@ -14,7 +15,8 @@ class MySokoban:
 
         gamemap_height = self.part * self.height
         gamemap_width = self.part * self.width
-        self.sreen = pygame.display.set_mode((gamemap_width, gamemap_height + self.part))
+        self.sreen = pygame.display.set_mode(
+            (gamemap_width, gamemap_height + self.part))
 
         self.loop()
 
@@ -23,18 +25,15 @@ class MySokoban:
         for name in ["floor", "wall", "place", "ball", "player", "valmis", "kohderobo"]:
             self.images.append(pygame.image.load(name + ".png"))
 
-
     def game(self):
         self.map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                       [1, 4, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1],
-                       [1, 0, 0, 3, 1, 0, 3, 0, 0, 3, 0, 1, 1, 0, 1, 1],
-                       [1, 0, 0, 0, 1, 3, 1, 1, 3, 3, 0, 1, 2, 2, 2, 1],
-                       [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 1],
-		                [1, 0, 3, 0, 3, 1, 3, 0, 1, 1, 1, 1, 0, 0, 2, 1],
-                       [1, 0, 1, 1, 0, 0, 0, 0, 0, 3, 0, 0, 2, 2, 2, 1],
-                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
-        self.siirrot = 0    
-
+                    [1, 4, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1],
+                    [1, 0, 0, 3, 1, 0, 3, 0, 0, 3, 0, 1, 1, 0, 1, 1],
+                    [1, 0, 0, 0, 1, 3, 1, 1, 3, 3, 0, 1, 2, 2, 2, 1],
+                    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 1],
+                    [1, 0, 3, 0, 3, 1, 3, 0, 1, 1, 1, 1, 0, 0, 2, 1],
+                    [1, 0, 1, 1, 0, 0, 0, 0, 0, 3, 0, 0, 2, 2, 2, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
     def events(self):
         for command in pygame.event.get():
@@ -52,21 +51,20 @@ class MySokoban:
             if command.type == pygame.QUIT:
                 exit()
 
-
-    def move(self, y, x):
+    def move(self, move_y, move_x):
         if self.peli_lapi():
             return
 
         old_player_y, old_player_x = self.find()
-        new_player_y = old_player_y + y
-        new_player_x = old_player_x + x
+        new_player_y = old_player_y + move_y
+        new_player_x = old_player_x + move_x
 
         if self.map[new_player_y][new_player_x] == 1:
             return
 
         if self.map[new_player_y][new_player_x] in [3, 5]:
-            new_ball_y = new_player_y + y
-            new_ball_x = new_player_x + x
+            new_ball_y = new_player_y + move_y
+            new_ball_x = new_player_x + move_x
 
             if self.map[new_ball_y][new_ball_x] in [1, 3, 5]:
                 return
@@ -77,35 +75,35 @@ class MySokoban:
         self.map[old_player_y][old_player_x] -= 4
         self.map[new_player_y][new_player_x] += 4
 
-        self.siirrot += 1
-
     def find(self):
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.map[y][x] in [4, 6]:
-                    return (y, x)
+        for column in range(self.height):
+            for row in range(self.width):
+                if self.map[column][row] in [4, 6]:
+                    return (column, row)
 
     def display_game(self):
         self.sreen.fill((100, 0, 255))
 
-        for y in range(self.height):
-            for x in range(self.width):
-                ruutu = self.map[y][x]
-                self.sreen.blit(self.images[ruutu], (x * self.part, y * self.part))
+        for column in range(self.height):
+            for row in range(self.width):
+                ruutu = self.map[column][row]
+                self.sreen.blit(
+                    self.images[ruutu], (row * self.part, column * self.part))
 
         pygame.display.flip()
 
     def peli_lapi(self):
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.map[y][x] in [2, 6]:
+        for column in range(self.height):
+            for row in range(self.width):
+                if self.map[column][row] in [2, 6]:
                     return False
         return True
-    
+
     def loop(self):
         while True:
             self.events()
             self.display_game()
+
 
 if __name__ == "__main__":
     MySokoban()
