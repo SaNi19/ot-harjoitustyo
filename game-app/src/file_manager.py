0b0cc71269e94b8b0295a759c8b5e8ce7a1d3b23
Sari <sarimarjaana@gmail.com>
2023-05-01 13:26:45 +0300

    lisätty tiedostoihin docstring-dokumentaatiota

--- game-app/src/file_manager.py
+++ game-app/src/file_manager.py
@@ -1,8 +1,20 @@
 class FileManager():
+    """Luokka, joka vastaa tiedostonhallinnasta.
+    """
     def __init__(self, data):
+        """Luokan konstruktori, joka luo yhteyden tiedostoon data.txt.
+
+        Args:
+            data (txt): data.txt
+        """
         self.__data = data
 
     def load(self):
+        """Lukee tiedostoa.
+
+        Returns:
+            List: Palaajat ja tulokset.
+        """
         names = {}
         with open(self.__data) as file:
             for row in file:
@@ -13,6 +25,11 @@ class FileManager():
         return names
 
     def save_result(self, data: dict):
+        """Kirjoittaa tiedostoon.
+
+        Args:
+            data (dict): Pelaajan nimi ja tulos.
+        """
         with open(self.__data, "w") as file:
             for name, results in data.items():
                 row = [name] + results
--- game-app/src/ui.py
+++ game-app/src/ui.py
@@ -3,7 +3,11 @@ from results_service import ResultsService
 
 
 class UI:
+    """Luokka, joka vastaa ohjelman käyttöliittymästä
+    """
     def __init__(self):
+        """Luokan konstruktori, joka luo tuloslistan ja tiedostonhallinnan.
+        """
         self.__results_list = ResultsService()
         self.__filemanager = FileManager("data.txt")
 
@@ -13,6 +17,8 @@ class UI:
 
 
     def help(self):
+        """Näyttää käyttäjälle ohjelman valikkotoiminnot.
+        """
         print("commands: ")
         print("0 exit")
         print("1 add result")
@@ -20,6 +26,8 @@ class UI:
         print("3 all results")
 
     def run(self):
+        """Käynnistää ohjelman valikon ja kutsuu metodeita käyttäjän valinnan mukaan.
+        """
         self.help()
         while True:
             print("")
@@ -36,20 +44,31 @@ class UI:
 
 
     def add_game_result(self):
+        """Kysyy käyttäjän nimen ja tuloksen, ja tallentaa tuloksen tiedostoon.
+        """
         name = input("name: ")
         result = input("result: ")
         self.__results_list.add_result(name, result)
 
     def get_result(self):
+        """Kysyy käyttäjältä nimeä ja hakee tiedostosta nimeä vastaavan tuloslistan.
+        Jos nimeä vastaavia tuloksia ei löydy, palauttaa viestin: "No results yet".
+        """
         name = input("name: ")
         results = self.__results_list.get_result(name)
         if results is None:
-            print("No aresults yet")
+            print("No results yet")
             return
         for reg in results:
             print(reg)
 
     def all_result(self):
+        """Tulostaa listan kaikista pelaajista ja pelaajien tuloksista.
+        Jos tuloksia ei löydy, palauttaa viestin: "No results yet".
+
+        Args:
+            List: Pelaajat ja tulokset
+        """
         all_results = self.__results_list.all_results()
         if all_results is None:
             print("No results yet")
@@ -58,6 +77,8 @@ class UI:
         print(self.__results_list.all_results)
 
     def end_app(self):
+        """Tallentaa kaikki tulokset ja sulkee ohjelman.
+        """
         self.__filemanager.save_result(self.__results_list.all_results())
 
 
