96429ab5a9f8f70cdd015a0c56e24f17dd07787e
Sari <sarimarjaana@gmail.com>
2023-05-13 20:51:28 +0300

    peliä muokattu

--- game-app/src/game_services.py
+++ game-app/src/game_services.py
@@ -2,26 +2,46 @@ import sqlite3
 
 
 class GameServices:
-    def __init__(self, get_result, start):
+    """Luokka, jonka avulla tulos tallennetaan tietokantaa
+    ja etsitään tulos, jossa on käytetty vähiten askeleita.
+    """
+
+    def __init__(self, get_result):
+        """Luokan kostruktori, joka luo uuden tuloksen.
+
+        Args:
+            get_result: Pelissä käytetyt askeleet.
+        """
         self.data = sqlite3.connect("resultlist.db")
         self.get_result = get_result
-        self.start = start
         self.data.isolation_level = None
+        """Tarkistaa, onko tietokanta jo luotu. Jos tietokanta on valmiina,
+        palauttaa teksitn "Table is created".
+        """
         try:
             self.data.execute(
                 "CREATE TABLE Resultlist (id INTEGER PRIMARY KEY, name TEXT, result INTEGER)")
         except:
             print("Table is created")
 
-    def add_game_result(self, game_result):
-        # name = input("name: ")
-        name = "player"
-        # result = input("result: "
+    def add_game_result(self, user_name, game_result):
+        """Lisäää pelaajan nimen ja pelissä käytetyt askeleet tietokantaan.
+
+        Args:
+            user_name: Käyttäjän nimi.
+            game_result: Pelissä käytetyt askeleet.
+        """
+        name = user_name
         result = game_result
         self.data.execute(
             "INSERT INTO Resultlist (name, result) VALUES (?,?)", [name, result])
 
     def best_result(self):
+        """Etsii pelattujen pelien tuloksen, jossa on käytetty vähiten askeleita.
+        Jos tietokannasta löytyy tulos, palauttaa tuloksen. 
+        Jos vielä ei ole tuloksia, palauttaa viestin "No results yet"
+
+        """
         best = self.data.execute(
             "SELECT MIN(result) FROM Resultlist").fetchone()
         if best:
--- game-app/src/move_player.py
+++ game-app/src/move_player.py
@@ -1,8 +1,22 @@
 
 
 class MovePlayer:
+    """Luokka, jonka avulla siirretään pelihahmoa ja palloja.
+    """
 
-    def __init__(self, map, height, width, step, move_y, move_x, find):
+    def __init__(self, map, height, width, step, move_y, move_x, find, game_end):
+        """Luokan konstruktori, joka luo pelihahmolle ja pallolle uuden sijainnin.
+
+        Args:
+            map (_type_): _description_
+            height (_type_): _description_
+            width (_type_): _description_
+            step (_type_): _description_
+            move_y (_type_): _description_
+            move_x (_type_): _description_
+            find (_type_): _description_
+            game_end (_type_): _description_
+        """
         self.self = self
         self.map = map
         self.height = height
@@ -11,30 +25,58 @@ class MovePlayer:
         self.move_y = move_y
         self.move_x = move_x
         self.find = find
+        self.game_end = game_end
 
     def move(self, move_y, move_x):
+        """
+
+        Args:
+            move_y: Siirtymä x-akselilla.
+            move_x: Siirtymä y-akselilla.
+        """
         wall = 1
         ball = 3
         end = 5
+
+        """Tarkistaa onko kaikki pallot varastossa.
+        """
         if self.game_end():
             return
+
+        """Esii pelihahmon sijainnin ja antaa pelihahmolle uude x- ja y-arvot.
+        """
 
         old_player_y, old_player_x = self.find()
         new_player_y = old_player_y + move_y
         new_player_x = old_player_x + move_x
 
+        """Tarkistaa onko pelihahmon siirtymäsuunnassa seinä.
+        """
+
         if self.map[new_player_y][new_player_x] == wall:
             return
+
+        """Jos pelihamo voi liikkua, asettaa palolle uudet x- ja y-arvot.
+        """
 
         if self.map[new_player_y][new_player_x] in [ball, end]:
             new_ball_y = new_player_y + move_y
             new_ball_x = new_player_x + move_x
 
+            """Tarkistaa onko pallo varastossa.
+            """
+
             if self.map[new_ball_y][new_ball_x] in [wall, ball, end]:
                 return
 
+            """Asettaa pelihamolle ja palolle uudet x- ja y-arvot
+            """
+
             self.map[new_player_y][new_player_x] -= 3
             self.map[new_ball_y][new_ball_x] += 3
+
+        """Siirtää pelihamo pelilaudalla uuteen sijaintiin ja lisää askeleen.
+        """
 
         self.map[old_player_y][old_player_x] -= 4
         self.map[new_player_y][new_player_x] += 4
